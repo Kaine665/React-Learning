59b765cb92730b788799d02c663253871e106565
Kaine <155346835+1367-5@users.noreply.github.com>
2024-12-22 21:00:18 +0800

    chaggenge2 写添加项目功能

--- src/App.js
+++ src/App.js
@@ -3,26 +3,18 @@ import React from "react";
 import Content from "./Content";
 import Footer from "./Footer";
 import Header from "./Header";
-import AddItem from "./AddItem";
 
 function App() {
   // content data and function
   const [items, setItems] = React.useState(
     JSON.parse(localStorage.getItem("shoppinglist"))
   );
-  const [newItem, setNewItem] = React.useState("");
 
   const setAndSaveItems = (newListItems) => {
     setItems(newListItems);
     localStorage.setItem("shoppinglist", JSON.stringify(newListItems));
   };
 
-  const addItem = (item) => {
-    const id = items.length ? items[items.length - 1].id + 1 : 1;
-    const myNewItem = { id: id, checked: false, item: item };
-    const listItems = [...items, myNewItem];
-    setAndSaveItems(listItems);
-  };
   const handleCheck = (id) => {
     const listItems = items.map((item) =>
       item.id === id ? { ...item, checked: !item.checked } : item
@@ -35,22 +27,10 @@ function App() {
     setAndSaveItems(listItems);
     localStorage.setItem("shoppinglist", JSON.stringify(listItems));
   };
-  const handleSubmit = (e) => {
-    e.preventDefault();
-    if (!newItem) return;
-    addItem(newItem);
-    setNewItem("");
-    console.log("submitted");
-  };
 
   return (
     <div className="App">
       <Header title={"Hello"} />
-      <AddItem
-        newItem={newItem}
-        setNewItem={setNewItem}
-        handleSubmit={handleSubmit}
-      />
       <Content handleCheck={handleCheck} handleDelete={handleDelete} />
       <Footer length={items.length} />
     </div>
